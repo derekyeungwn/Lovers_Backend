205e7b4f08c456ebb6b35b1e34f6cee7e303e12a
Derek Yeung <derekyeungwn@gmail.com>
2022-02-21 21:16:42 +0800

    modify appCode

--- resources/appCode.py
+++ resources/appCode.py
@@ -1,19 +1,30 @@
 import datetime, json
 from flask import request, Response, Blueprint
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from .db import db
 
 appCode_bp = Blueprint('appCode', __name__)
 
 @appCode_bp.route('/', methods=['GET'])
+@jwt_required()
 def getAppCode(): 
     output = []
+    thisdict = {}
     for data in db.app_code.find():
-        thisdict = {}
         for item in data:            
             if item != '_id': 
                 if isinstance(data[item], datetime.datetime):                    
                     thisdict[item] = data[item].strftime('%d/%m/%Y')
                 else:
                     thisdict[item] = data[item]
-        output.append(thisdict)
+
+    user_id = get_jwt_identity()
+    filter = {}
+    filter["user_id"] = user_id
+    result = db.users.find_one(filter)
+    thisdict['user_name'] = result['user_name']
+    thisdict['user_id'] = result['user_id']
+
+    output.append(thisdict)
+
     return Response(json.dumps({'data' : output}), mimetype='application/json', status=200)
